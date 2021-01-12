6ad0f94fbb99a308b147c2a9046f5f4a4665fceb
tretap05 <s6302041620087@email.kmutnb.ac.th>
2021-01-12 07:24:17 +0700

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// ส่วนหัวสำหรับดึก Library (1)
+// การกด ctrl + / คือการใส่คอมเม้นต์แบบเร็ว short cut key
+
+// ส่วน function หลัก (2)
+int main()
+{
+    printf("name : Tretap Khovintavong\nnickname : TiTee\nage : 18\nSomething like : Candy\nWhat I want to do : Sleep \nHappiness is : Living with family");
+    /* 
+        ส่วนคำสั่งการทำงาน (3)
+        printf คือ ฟังก์ชันที่ไว้สำหรับเเสดงผลทางหน้าจอ
+    */
+    // วิธีการจัดชุดคำสั่งให้สวย ให้ shift + alt + f
+    // การเปิด Terminal ให้กด ctrl + j
+    // cmd
+}
